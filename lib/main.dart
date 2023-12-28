bfefcf271bddb008d8600ca4412f99b7c82cc83d
Amangulati05 <131166446+Amangulati05@users.noreply.github.com>
2023-12-28 11:08:26 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,63 @@
 import 'package:flutter/material.dart';
-void main () {
-  runApp(MyWeatherApp());
 
+void main() {
+  runApp(WeatherApp());
 }
- class MyWeatherApp extends StatelessWidget{
-  @override
-  Widgetbuild(BuildContext context) {
-    return MaterialApp(
-      title: 'WeatherApp',
-      theme: ThemeData (
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity
 
+class WeatherApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      home: WeatherScreen(),
+      home: WeatherHomePage(),
     );
   }
-
 }
 
-class WeatherScreen extends StatelessWidget {
+class WeatherHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('MyWeatherApp'),
-          centerTitle: true,
-
-        ),
-        body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Ambala',
-                  style: TextStyle(
-                    fontSize: 24.0,
-                    fontWeight: FontWeight.bold,
-
-
-                  ),
-                ),
-
-                SizedBox(height: 20.0),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text('Humidity'),
-                    Text('80%'),
-
-
-                  ],
-                ),
-                Column(
-                  children: [
-                    Text('pressure'),
-                    Text('1012 hPa'),
-
-                  ],
-                ),
-              ]
-          ),
-        )
-
+      appBar: AppBar(
+        title: Text('Ambala Weather'),
+      ),
+      body: WeatherInfo(),
     );
   }
+}
+
+class WeatherInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Ambala',
+            style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Icon(
+            Icons.wb_sunny, // Replace with appropriate weather icon
+            size: 100,
+            color: Colors.orange,
+          ),
+          SizedBox(height: 20),
+          Text(
+            '24°C', // Replace with actual temperature
+            style: TextStyle(fontSize: 48),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Partly Cloudy', // Replace with actual weather description
+            style: TextStyle(fontSize: 24),
+          ),
+        ],
+      ),
+    );
+  }
+}
